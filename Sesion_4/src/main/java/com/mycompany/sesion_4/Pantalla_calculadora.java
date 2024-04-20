5d9e9fab8d8189b9a86343cda4b656b2f888da0a
PXamir <166246624+PXamir@users.noreply.github.com>
2024-04-20 14:12:34 -0500

    Add files via upload

---
+++ Sesion_4/src/main/java/com/mycompany/sesion_4/Pantalla_calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sesion_4;
+
+import Pantallas.Pantalla_Calculadora;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Pantalla_calculadora extends Pantalla_Calculadora {
+
+    public Pantalla_calculadora() {
+    }
+    
+}
---
+++ Sesion_4/src/main/java/com/mycompany/sesion_4/Sesion_4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sesion_4;
+
+import Pantallas.Pantalla_Calculadora;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Sesion_4 {
+
+    public static void main(String[] args) {
+        
+        Pantalla_Calculadora pantalla = new Pantalla_calculadora();
+        pantalla.setVisible(true);
+        
+    }
+}
